6770d5af7ca9d5db136bd7f28a9bcec777d11eed
darryl west <dpw@raincitysoftware.com>
2023-02-03 10:01:54 -0800

    trig

---
+++ trig.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# dpw@Darryls-iMac.localdomain
+# 2023-02-03 17:57:48
+#
+
+import math
+
+theta = math.pi / 3
+
+print('py: ', math.pi, ', theta: ', theta)
+
+cosv = math.cos(theta)
+sinv = math.sin(theta)
+tanv = math.tan(theta)
+
+print('cos, sin, tan: ', cosv, sinv, tanv)
+
