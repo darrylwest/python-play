98b6b3c16f0b5cac1f25d8c64c5b11e77d6e1297
darryl west <dpw@raincitysoftware.com>
2023-02-03 13:19:57 -0800

    updates for plots

---
+++ cubic-spline-interp.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# dpw@Darryls-iMac.localdomain
+# 2023-02-03 21:08:32
+#
+
+from scipy.interpolate import CubicSpline
+import numpy as np
+import matplotlib.pyplot as plt
+
+plt.style.use('seaborn-poster')
+
+x = [0, 1, 2]
+y = [1, 3, 2]
+
+# use bc_type = 'natural' adds the constraints as we described above
+f = CubicSpline(x, y, bc_type='natural')
+x_new = np.linspace(0, 2, 100)
+y_new = f(x_new)
+
+plt.figure(figsize = (10,8))
+plt.plot(x_new, y_new, 'b')
+plt.plot(x, y, 'ro')
+plt.title('Cubic Spline Interpolation')
+plt.xlabel('x')
+plt.ylabel('y')
+plt.show()
+
