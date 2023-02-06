0628ac73cbb4d980e257992f5f517749f2ac528c
darryl west <dpw@raincitysoftware.com>
2023-02-06 10:59:51 -0800

    graphs

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Python Playground
 
-###### darryl.west | 2023-02-03
+
+## Google Colaboratory
+
+[colab](https://colab.research.google.com/)
+
+###### darryl.west | 2023-02-05
---
+++ factor.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# dpw@Darryls-iMac.localdomain
+# 2023-02-05 22:40:03
+#
+
+import sympy
+from sympy import *
+
+var('x y')
+
+eq = x**3 - 2 * x**2 - 5*x + 6
+
+f = sympy.factor(eq)
+print(eq, " = ", f)
+
