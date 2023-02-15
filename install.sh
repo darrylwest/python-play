6ee72cc4dee52180c658ecdaf9ee6b2686fd0b75
darryl west <dpw@raincitysoftware.com>
2023-02-15 07:18:27 -0800

    python installations

---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+# dpw@BCT-MBP.localdomain
+# 2023-02-15 15:13:31
+#
+
+set -eu
+
+python3 -m pip install --upgrade pip
+
+packages="numpy sympy scipy matplotlib"
+
+for p in $packages
+do
+    python3 -m pip install $p
+done
+
+exit $?
+
