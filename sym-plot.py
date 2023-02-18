ffd41b12e2612fe67b27568dffea2815f1896e6c
darryl west <dpw@raincitysoftware.com>
2023-02-18 08:50:20 -0800

    improved symplot

--- sym-plot.py
+++ sym-plot.py
@@ -2,13 +2,25 @@
 # dpw@Darryls-iMac.localdomain
 # 2023-02-18 16:22:49
 #
+
 import numpy as np
 import sympy as sym
+import sympy.plotting as spl
 import math
 
 sym.var('x')
 
 y = x**2
 
-p = sym.plotting.plot(y)
+p = spl.plot(y, show=False)
 
+p.xlim = [-3, 8]
+p.ylim = [0, 50]
+
+p.title = "My Simple Sympy Plot"
+
+p[0].label = "y = x^2"
+p[0].line_color = 'r'
+p.legend = True
+
+p.show()
