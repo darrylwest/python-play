f8e5f8155f5a0d55d4d73bc1b42a9e8c9e5230c2
darryl west <dpw@raincitysoftware.com>
2023-02-24 15:50:06 -0800

    gym

--- is-prime.py
+++ is-prime.py
@@ -4,18 +4,16 @@
 #
 
 import argparse
-import sympy as sym
+import gmpy2 as gym
 from math import sqrt
 import random
 
-def prime(number): #prime function to check given number prime or not
-    p = sym.factorint(number)
-
-    return len(p) == 1
+def isprime(number): #prime function to check given number prime or not
+    return gym.is_prime(number)
 
 def find_large_prime():
     n = random.randrange(1000000000, 1000000000000)
-    while prime(n) == False:
+    while isprime(n) == False:
         n = n + 1
 
     return n
@@ -27,7 +25,7 @@ args = parser.parse_args()
 
 primes = []
 for n in args.integers:
-    if prime(n):
+    if isprime(n):
         primes.append(n)
     else:
         print(n, 'not prime')
