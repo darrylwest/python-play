f97892ee26320fa93a90757112f6345b4620ac36
darryl west <dpw@raincitysoftware.com>
2023-03-04 15:49:46 -0800

    file

---
+++ file-writer.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# dpw@Darryls-iMac.localdomain
+# 2023-03-03 21:03:10
+#
+import datetime
+
+filename='/tmp/junk.txt'
+
+# using 'with' ensures the file closes correctly...
+with open(filename, 'w') as f:
+    f.write(f"this is a test from me sent {datetime.datetime.now()}\n")
+    f.write('bye now...')
+
+print(f"file updated : {filename}")
+
