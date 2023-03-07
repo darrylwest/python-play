e7375a78cc5ba3b9ca1a2d33fde3d4b45c0e71e4
darryl west <dpw@raincitysoftware.com>
2023-03-07 13:43:58 -0800

    added toy web app with flask

---
+++ webapp.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# dpw@Darryls-iMac.localdomain
+# 2023-03-07 21:25:22
+#
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.get("/")
+def index():
+    return "<p>hello world</p>"
+
+
+@app.get("/what")
+def what():
+    return "<p>hello what?</p>"
+
+if __name__ == '__main__':
+    print("USE: FLASK_APP=webapp flask run")
