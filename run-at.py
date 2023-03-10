57d42bdbf9f91bb0dff1fd43755d764c9467b0f3
darryl west <dpw@raincitysoftware.com>
2023-03-10 14:04:24 -0800

    added schedule tests

---
+++ run-at.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# dpw@Darryls-iMac.localdomain
+# 2023-03-10 21:45:37
+#
+import schedule
+import time
+import threading
+
+
+def job():
+    print(f'working at {time.time()} on thread {threading.current_thread()}')
+
+def run_threaded(func):
+    jthread = threading.Thread(target=func)
+    jthread.start()
+
+schedule.every(5).seconds.do(run_threaded, job)
+
+n = 60
+while n > 0:
+    schedule.run_pending()
+    time.sleep(1)
+    n += 1
+
