72eb9933410dfbaf3df33f0a8d89cce2bb9882a0
darryl west <dpw@raincitysoftware.com>
2023-03-13 10:38:19 -0700

    added line stats to calc length

--- line-stats.py
+++ line-stats.py
@@ -5,33 +5,30 @@
 
 import argparse
 import numpy as np
-import sympy as sym
+import subprocess
 import math
 
 parser = argparse.ArgumentParser(description='Enter the line start/end as x1 y1 x2 y2')
-parser.add_argument('numbers', metavar='4', type=float, nargs='+', help='the line start/end as x1 y1 x2 y2') 
+parser.add_argument('numbers', metavar='4', type=float, nargs='+', help='the start/end points as x1 x2 x3 x4')
 
 args = parser.parse_args()
 
-if len(args.numbers) != 4:
-    print("Error: please enter the line start and end as x1 y1 x2 y2")
-else:
-    x1 = args.numbers[0]
-    y1 = args.numbers[1]
-    x2 = args.numbers[2]
-    y2 = args.numbers[3]
+x1 = args.numbers[0]
+y1 = args.numbers[1]
+x2 = args.numbers[2]
+y2 = args.numbers[3]
 
-    dx, dy = (x2 - x1), (y2-y1)
-    llen = math.hypot(dy, dx)
+dx, dy = (x2 - x1), (y2-y1)
+llen = math.hypot(dy, dx)
 
-    midx = (x1+x2)/2
-    midy = (y1+y2)/2
+midx = (x1+x2)/2
+midy = (y1+y2)/2
 
-    m = dy / dx
-    b = y1 - (m*x1)
-    xi = (y1 - b)/m
+m = dy / dx
+b = y1 - (m*x1)
+xi = (y1 - b)/m
 
-    print(f'The midpoint for the line at {x1},{y1} - {x2},{y2} is {midx},{midy}')
-    print(f'The slope of the line = {m}, y intercept (b) = {b}, the x intercept is {xi}')
-    print(f'The line length is sqrt({dx**2 + dy**2}) = {llen}')
+print(f'The midpoint for the line at {x1},{y1} to {x2},{y2} is {midx},{midy}')
+print(f'The slope of the line = {m}, y intercept (b) = {b}, the x intercept is {xi}')
+print(f'The line length is sqrt({dx**2 + dy**2}) = {llen}')
 
