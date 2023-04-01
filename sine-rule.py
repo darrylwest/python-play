803bb51975ed73387bfbbf43cc5a143e763038de
darryl west <dpw@raincitysoftware.com>
2023-04-01 14:52:27 -0700

    sine rule updates

--- sine-rule.py
+++ sine-rule.py
@@ -1,20 +1,31 @@
 #!/usr/bin/env python3
 # dpw@Darryls-iMac.localdomain
-# 2023-03-30 22:33:18
+# 2023-04-01 21:30:55
 #
 import math
 
 
-#
-# sine rule with two angles and a known side
-#
-def cos_rule_sas(b, A, c):
-    angle = math.radians(A)
-    a = math.sqrt(b**2 + c**2 - 2*b*c * math.cos(angle))
+# implement two sine rules, angle, side, angle -> side and angle, side, side -> angle (acute)
 
-    return a
+#
+# returns the length of b opposite B
+# A = angle in degrees
+# a = side opposite A
+# B = angle in degrees opposite b
+#
+def sin_rule_asa(A, a, B):
+    aa = math.radians(A)
+    ab = math.radians(B)
+    return (math.sin(ab) * a) / math.sin(aa)
 
 if __name__ == "__main__":
-    b, A, c = (736, 3, 915)
-    print(f"b={b}, A={A}, c={c} = {cos_rule_sas(5, 61, 16)}")
+    A, a, B = (58, 14, 75)
+    b = sin_rule_asa(A, a, B)
+
+    print(f"{A}, {a}, {B} = {b}") 
+
+    A, a, B = (46, 12, 58)
+    b = sin_rule_asa(A, a, B)
+
+    print(f"{A}, {a}, {B} = {b}") 
 
