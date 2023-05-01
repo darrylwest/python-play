7abd4541707a9c4a3dc091950dab65e3b15e8948
darryl west <dpw@raincitysoftware.com>
2023-05-01 06:29:19 -0700

    piecewise

---
+++ piecewise.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# dpw@Darryls-iMac.localdomain
+# 2023-04-29 14:13:23
+#
+import numpy as np
+import sympy as sym
+import matplotlib.pyplot as plt
+from IPython.display import display,Math
+from sympy.abc import x
+
+piece1 = x**2
+piece2 = -2*x
+piece3 = x**3/10
+
+fx = sym.Piecewise((piece1,x<0),(piece2, (x>=0) & (x<3)), (piece3,x>=3))
+
+fxx = sym.lambdify(x, fx)
+xx = np.linspace(-3, 5, 1000)
+
+plt.plot(xx, fxx(xx))
+plt.grid()
+
+plt.show()
