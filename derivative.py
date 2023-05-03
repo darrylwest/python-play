826e544fa87e1c64874ac49ef927f3e2f491656e
darryl west <dpw@raincitysoftware.com>
2023-05-03 05:44:02 -0700

    der

---
+++ derivative.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# dpw@Darryls-iMac.localdomain
+# 2023-05-02 19:07:03
+#
+import numpy as np
+import sympy as sym
+import math
+
+x = sym.symbols('x')
+
+fx = 7**(x**2 - x)
+fp = sym.diff(fx)
+
+print(fx, " = ", fp)
