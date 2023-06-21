fbb722a6c3c2ba4f21310d03f9846b2e4e532dee
darryl west <dpw@raincitysoftware.com>
2023-06-21 10:18:11 -0700

    added begins to install list

--- install.sh
+++ install.sh
@@ -12,7 +12,7 @@ python3 -m pip install --upgrade pip
 # repl: ptpython
 # pip3 install --upgrade jupyterlab-vim
 
-packages="numpy sympy scipy matplotlib seaborn plotly pandas bokeh dask pymc3 statsmodels sklearn gmpy2 redis schedule requests httpx pyvibe"
+packages="numpy sympy scipy matplotlib seaborn plotly pandas begins bokeh dask pymc3 statsmodels sklearn gmpy2 redis schedule requests httpx pyvibe"
 
 for p in $packages
 do
