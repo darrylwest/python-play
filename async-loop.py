6abba235cf0cb153d21afe711b21cf8cbec2c754
darryl west <dpw@raincitysoftware.com>
2023-06-21 14:07:04 -0700

    added asyncio tests

---
+++ async-loop.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# dpw@plaza.localdomain
+# 2023-06-21 20:56:11
+
+import asyncio
+import datetime
+
+async def loop():
+    while True:
+        nxt = asyncio.sleep(2)
+
+        print(f'this is my job: {datetime.datetime.now()}')
+
+        await nxt
+
+
+if __name__ == "__main__":
+    asyncio.run(loop())
+
--- tornado-test.py
+++ tornado-test.py
@@ -2,12 +2,30 @@
 # dpw@plaza.localdomain
 # 2023-06-21 20:28:42
 
+#
+# TODO: add tornado.httpclient for client tests with AsyncHTTPClient
+#
+
 import asyncio
 import tornado
+import pyvibe as pv
+
+def home_page():
+    page = pv.Page("Vibe Generated")
+    page.add_header("My Web Page")
+    page.add_text("This is my first home page.")
+
+    with page.add_card() as card:
+        card.add_header("My Special Notice Card")
+        card.add_text("This is the body text of the notice.")
+        card.add_link("More to learn here", "https://www.pyvibe.com/component_reference.html")
+
+    return page.to_html()
+
 
 class Handler(tornado.web.RequestHandler):
     def get(self):
-        self.write("hello tornado")
+        self.write(home_page())
 
 def make_app():
     return tornado.web.Application([ (r"/", Handler), ])
