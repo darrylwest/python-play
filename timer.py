761b4c7318d85a8ac19059bef1f5a1b995531641
darryl west <dpw@raincitysoftware.com>
2023-07-02 18:52:10 -0700

    added notes to timer decorator

--- timer.py
+++ timer.py
@@ -2,6 +2,8 @@
 # dpw@plaza.localdomain
 # 2023-07-03 01:37:23
 
+# see https://medium.com/@mkuhikar/python-decorators-advanced-67420a5b7278 for notes on Decorators
+
 import begin
 import time
 
@@ -10,16 +12,20 @@ def timer(func):
         start_time = time.time()
         result = func(*args, **kwargs)
         end_time = time.time()
-        print(f'{func.__name__}() runtime: {end_time - start_time} seconds')
+        print(f'{func.__name__}() runtime: {end_time - start_time} seconds', flush=True)
         return result
 
     return wrapper
 
 @timer
-def myfn():
-    time.sleep(2.5)
-    return 'hello'
+def myfn(i):
+    time.sleep(0.2)
+    return 'hello ' + str(i)
 
 @begin.start
 def main(arg1 = None):
-    myfn()
+    print('simulates a long-running function to demonstrate how to create and use a Decorator...')
+
+    for i in range(10):
+        myfn(i+1)
+
