b8df906f8d41c09b512287f68c19bd80e3eddcf4
darryl west <dpw@raincitysoftware.com>
2023-07-05 09:00:15 -0700

    shortener

---
+++ short-url.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# dpw@plaza.localdomain
+# 2023-07-05 15:22:54
+
+# ref: https://pyshorteners.readthedocs.io/en/latest/
+# TODO: must have a valid set of API keys...
+
+import begin
+import pyshorteners
+
+s = 
+
+@begin.start
+def main(arg1 = None):
+    print(f'hello {arg1}')
