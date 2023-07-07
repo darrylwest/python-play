ee72b192775dba4b84fb5e50b7ba20aab0203595
darryl west <dpw@raincitysoftware.com>
2023-07-07 10:11:13 -0700

    calc phi

---
+++ calc-phi.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# dpw@plaza.localdomain
+# 2023-07-07 17:09:08
+
+import begin
+import math
+import random
+
+def calc_phi():
+# the random start value
+    phi = (1 + math.sqrt(5)) / 2
+    x = random.uniform(-10.0, 10.0)
+    print(f'start number = {x}')
+
+    count = 50
+    for n in range(count):
+        x = 1 + 1.0 / x
+
+    print(f'Phi calculation result: {x} after {count} rounds, error: {phi - x}...')
+
+@begin.start
+def main(arg1 = None):
+    calc_phi()
