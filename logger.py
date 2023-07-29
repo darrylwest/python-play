75a0fa91ad0460db54d36a3f0f044ee4d10aa6d7
darryl west <dpw@raincitysoftware.com>
2023-07-29 07:08:59 -0700

    added helpers to logger

--- logger.py
+++ logger.py
@@ -3,21 +3,48 @@
 # 2023-02-19 13:48:27
 #
 
+# @see https://docs.python.org/3/library/logging.html for the python docs
 # @see https://docs.python.org/3/howto/logging-cookbook.html for improved formatting, file rotation, etc.
 
-import logging as log
+import logging
+
+log = logging.getLogger('app')
+log.setLevel(logging.NOTSET)
 
 fname='test.log'
-log.basicConfig(filename=fname, filemode='w', level=log.DEBUG)
 
-def main():
+def get_logger():
+    return log
+
+def init_stream_logger(logger):
+    handler = logging.StreamHandler()
+    handler.setLevel(logging.DEBUG)
+
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    handler.setFormatter(formatter)
+
+    logger.addHandler(handler)
+
+def init_file_logger(logger):
+    handler = logging.FileHandler(fname)
+    handler.setLevel(logging.DEBUG)
+
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    handler.setFormatter(formatter)
+
+    log.addHandler(handler)
+
+def test():
     log.debug("a debug test")
-    log.info('this is a test...')
+    log.info('this is an into test...')
     log.warning('warning you')
     log.error('this is an error')
+    log.critical('this is CRITICAL')
 
-    print('look at the logfile %s' %{fname})
+    print(f'look at the logfile {fname}')
 
 
 if __name__ == '__main__':
-    main()
+    init_stream_logger(log)
+    init_file_logger(log)
+    test()
