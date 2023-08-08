8c0bcb70fea36d076e21920233945624fb1e0a6b
darryl west <dpw@raincitysoftware.com>
2023-08-08 11:56:15 -0700

    toml reader

---
+++ toml-reader.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# dpw@plaza.localdomain
+# 2023-08-08 17:43:25
+
+import begin
+import tomllib
+from pprint import pprint as pp
+
+def process():
+    with open("data.toml", "rb") as f:
+        data = tomllib.load(f)
+
+    pp(data)
+
+@begin.start
+def main(arg1 = None):
+    process()
