3dedad061ee66784b827792850f9063374ec6942
darryl west <dpw@raincitysoftware.com>
2023-08-15 08:04:41 -0700

    added plotting folder

--- one-line-file-read.py
+++ one-line-file-read.py
@@ -2,14 +2,9 @@
 # dpw@plaza.localdomain
 # 2023-06-11 15:33:29
 
-import numpy as np
-import sympy as sym
-
-# import argparse
-
 
 def main():
-    data = [line.strip() for line in open("xy.data", "r")]
+    data = [line.strip() for line in open("data/xy.data", "r")]
     print(data)
 
 
