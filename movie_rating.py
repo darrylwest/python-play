ef4db17e6ea3064d6551c9e5feb9167b0f74e2ee
darryl west <dpw@raincitysoftware.com>
2023-08-15 10:03:18 -0700

    static validate method

--- movie_rating.py
+++ movie_rating.py
@@ -6,27 +6,33 @@ import typer
 
 
 class Movie:
-    def __init__(self, r):
-        self._rating = r
+    def __init__(self, rating: int):
+        if Movie.validate(rating):
+            self._rating = rating
 
     @property
-    def rating(self):
+    def rating(self) -> int:
         return self._rating
 
     @rating.setter
-    def rating(self, value):
-        if 0 <= value <= 5:
+    def rating(self, value: int) -> None:
+        if Movie.validate(value) == True:
             self._rating = value
+
+    @staticmethod
+    def validate(value: int) -> bool:
+        if 0 <= value <= 5:
+            return True
         else:
             raise ValueError("rating must be between 1 and 5")
 
 
 def main(rating: int) -> None:
-    batman = Movie(1)
+    batman = Movie(rating)
 
     print(f"rating: {batman.rating}")
 
-    batman.rating = rating
+    batman.rating = 1
     print(f"rating: {batman.rating}")
 
     # this will throw
