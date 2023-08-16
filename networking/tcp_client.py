89bd9eb71e2ffc2b750fd36e85cfa955b66cc4db
darryl west <dpw@raincitysoftware.com>
2023-08-16 16:15:02 -0700

    tcp/udp prior to threads and async

--- networking/tcp_client.py
+++ networking/tcp_client.py
@@ -6,26 +6,29 @@ import typer
 from rich.console import Console
 
 import socket
+import threading
 
 console = Console()
+
 
 def connect(port: int = 14000):
     host = socket.gethostbyname(socket.gethostname())
 
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    console.log(f'server connecting to {host}:{port}')
+    console.log(f"server connecting to {host}:{port}")
 
     client.connect((host, port))
 
     return client
 
+
 def main() -> None:
     client = connect()
     data = client.recv(32).decode()
-    console.log(f'server-> {data}')
+    console.log(f"server-> {data}")
     client.close()
 
-if __name__ == '__main__':
-    typer.run(main)
 
+if __name__ == "__main__":
+    typer.run(main)
--- networking/tcp_server.py
+++ networking/tcp_server.py
@@ -6,8 +6,10 @@ import typer
 from rich.console import Console
 
 import socket
+import threading
 
 console = Console()
+
 
 def create_socket(port: int = 14000):
     host = socket.gethostbyname(socket.gethostname())
@@ -15,24 +17,25 @@ def create_socket(port: int = 14000):
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server.bind((host, port))
 
-    console.log(f'server bind to {host}:{port}')
+    console.log(f"server bind to {host}:{port}")
 
     return server
 
+
 def main() -> None:
-    console.log('create the server tcp socket...')
+    console.log("create the server tcp socket...")
     server = create_socket()
-    console.log(f'socket: {server}')
+    console.log(f"socket: {server}")
     server.listen()
 
     while True:
         client, address = server.accept()
-        console.log(f'connected to {address}...')
-        client.send('Ready'.encode('utf-8'))
+        console.log(f"connected to {address}...")
+        client.send("Ready".encode("utf-8"))
         break
 
     server.close()
 
-if __name__ == '__main__':
-    typer.run(main)
 
+if __name__ == "__main__":
+    typer.run(main)
