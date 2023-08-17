2d4c6e44dc798f71a6a1299aafb91d4eab336f95
darryl west <dpw@raincitysoftware.com>
2023-08-17 15:49:28 -0700

    uvloop async socket server

--- networking/uvloop_server.py
+++ networking/uvloop_server.py
@@ -4,35 +4,67 @@
 
 
 import asyncio
+import signal
 import uvloop
 
 
 async def handle_client(reader, writer):
-    data = await reader.read(100)
-    message = data.decode()
-    addr = writer.get_extra_info("peername")
+    try:
+        addr = writer.get_extra_info("peername")
+        while True:
+            data = await reader.read(1024)
+            if not data:
+                break
 
-    print(f"Received {message!r} from {addr!r}")
+            message = data.decode().rstrip()
 
-    print("Send: {!r}".format(message))
-    writer.write(data)
-    await writer.drain()
+            response = "ok".encode()
+            print(f"client-> {message} from {addr!r}, response: {response}")
 
-    print("Closing the connection")
-    writer.close()
+            writer.write(response)
+            await writer.drain()
 
+
+    except ValueError as err:
+        print(err)
+
+    except Exception as ex:
+        print(f'An exception was detected: {ex}')
+
+    finally:
+        print("closing the connection")
+        writer.close()
 
 async def main(port: int = 15000):
-    server = await asyncio.start_server(handle_client, "::1", port)
+    try:
+        server = await asyncio.start_server(handle_client, "::1", port)
 
-    addr = server.sockets[0].getsockname()
-    print(f"Serving on {addr}")
+        addr = server.sockets[0].getsockname()
+        print(f"Serving on {addr}")
 
-    async with server:
-        await server.serve_forever()
+        async with server:
+            await server.serve_forever()
 
+    except Exception as ex:
+        print(f'exception: {ex}')
+
+    finally:
+        print('out...')
 
 if __name__ == "__main__":
     loop = uvloop.new_event_loop()
     asyncio.set_event_loop(loop)
-    loop.run_until_complete(main())
+
+    def shutdown():
+        loop.stop()
+
+    loop.add_signal_handler(signal.SIGINT, shutdown)
+    loop.add_signal_handler(signal.SIGTERM, shutdown)
+
+    try:
+        print(f'run the loop with shutdown handler...')
+        loop.run_until_complete(main())
+    finally:
+        print(f'close the loop');
+        loop.close()
+
