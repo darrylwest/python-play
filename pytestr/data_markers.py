fb1825325377e3b05d367a6cd1f4a75096a561dc
darryl west <dpw@raincitysoftware.com>
2023-08-30 06:58:46 -0700

    pytest with setup, teardown and data fixtures

---
+++ pytestr/data_markers.py
@@ -0,0 +1,21 @@
+import pytest
+
+
+@pytest.fixture
+def fixt(request):
+    marker = request.node.get_closest_marker("fixt_data")
+    if marker is None:
+        print('marker data is None')
+        data = None
+    else:
+        data = marker.args[0]
+        print(f'marker data is {data}')
+
+    # Do something with the data
+    return data
+
+
+@pytest.mark.fixt_data(42)
+def test_fixt(fixt):
+    assert fixt == 42
+
