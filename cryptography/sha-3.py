bfc54c394c8c92fb30a8112acde8e633432ebb36
darryl west <dpw@raincitysoftware.com>
2023-09-01 08:45:56 -0700

    updates

--- cryptography/sha-3.py
+++ cryptography/sha-3.py
@@ -3,7 +3,7 @@
 # 2023-09-01 14:23:46
 
 import sys
-from rich import inspect
+from rich import print, inspect
 from dataclasses import dataclass
 from datetime import datetime
 from faker import Faker
@@ -21,6 +21,21 @@ class TestUser:
     created: datetime
     age: int
     description: str
+    friends: list
+
+
+def create_user():
+    user = TestUser(
+        id=fake.unique.random_int(),
+        name=fake.name(),
+        created=datetime.utcnow(),
+        age=fake.random_int(min=10, max=50),
+        description=fake.sentence(),
+        friends=[fake.first_name(), fake.first_name(), fake.first_name()],
+    )
+
+    print(user)
+    return user
 
 
 def test_hash(hasher, bytes):
@@ -30,20 +45,6 @@ def test_hash(hasher, bytes):
     print(f"hasher: {hasher.name}")
     print(f"bin length: {len(hashed)}", hashed)
     print(f"hex length: {len(hex)}", hex)
-
-
-def create_user():
-    user = TestUser(
-        id=fake.random_int(),
-        name=fake.name(),
-        created=datetime.utcnow(),
-        age=fake.random_int() % 40,
-        description=fake.sentence(),
-        # friends = [fake.first_name(),fake.first_name(),fake.first_name()],
-    )
-
-    inspect(user)
-    return user
 
 
 def test_object():
@@ -62,8 +63,15 @@ def test_message():
 
 
 def main(args: list) -> None:
-    # test_message()
-    test_object()
+    if len(args) == 0:
+        test_message()
+        test_object()
+
+    if "message" in args:
+        test_message()
+
+    if "obj" in args:
+        test_object()
 
 
 if __name__ == "__main__":
