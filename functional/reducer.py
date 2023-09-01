fde7d3ea3d3bcb49fe77c835f9d793afef98c3db
darryl west <dpw@raincitysoftware.com>
2023-09-01 13:58:12 -0700

    functional tools

--- functional/reducer.py
+++ functional/reducer.py
@@ -4,10 +4,25 @@
 
 import sys
 from rich import print
+from functools import reduce
+
+sys.path.append("/functional")
+from scientists import create_list
 
 
 def main(args: list) -> None:
     print(f"{args}")
+
+    year = 2023
+
+    add_age = lambda acc, sci: acc + (year - sci.born)
+
+    tage = reduce(add_age, create_list(), 0)
+    print("age sum:", tage)
+
+    # a better way is to use sum around a generator like this
+    tage = sum(year - s.born for s in create_list())
+    print("sum ages from generator: ", tage)
 
 
 if __name__ == "__main__":
