5c8a36102f372fafdf271805846bdb66c137226f
darryl west <darryl.west@raincitysoftware.com>
2023-09-14 14:53:53 -0700

    added notes to run at

--- concurrency/run-at.py
+++ concurrency/run-at.py
@@ -2,14 +2,20 @@
 # dpw@Darryls-iMac.localdomain
 # 2023-03-10 21:45:37
 #
+
+# @see https://schedule.readthedocs.io/
+
 import threading
 import time
 
 import schedule
 
 
+# no input args; with args, use functools.partial to define
 def job():
-    print(f"working at {time.time()} on thread {threading.current_thread()}")
+    print(
+        f"working at {time.time()} on thread {threading.current_thread()}", flush=True
+    )
 
 
 def run_threaded(func):
@@ -17,10 +23,10 @@ def run_threaded(func):
     jthread.start()
 
 
-schedule.every(5).seconds.do(run_threaded, job)
+schedule.every(5).seconds.do(run_threaded, job)  # or job, *args, **kwargs)
 
-n = 10
+n = 60
 while n > 0:
     schedule.run_pending()
     time.sleep(1)
-    n += 1
+    n -= 1
