f025736a8f321209b6af161b5163089652b612b6
darryl west <darryl.west@raincitysoftware.com>
2023-09-20 09:25:51 -0700

    whoosh wash out

---
+++ algorithms/whoosh_search.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+# dpw@plaza.localdomain
+# 2023-09-19 23:21:55
+
+import sys
+from rich import print, inspect
+from dataclasses import dataclass
+from whoosh.index import create_in, open_dir
+from whoosh.fields import *
+from whoosh.qparser import QueryParser
+
+# class MySchema(SchemaClass):
+
+# The results does not match documentation.
+
+
+@dataclass
+class SearchEngine:
+    index_dir = 'indexdir'
+    schema = Schema(title=TEXT(stored=True), path=ID(stored=True), content=TEXT)
+    # ix = create_in(index_dir, schema)
+    ix = open_dir(index_dir)
+
+    def index(self):
+        ix = self.ix
+        writer = ix.writer()
+        writer.add_document(title=u"First document", path=u"/a", content=u"This ithe first document we've added")
+        writer.add_document(title=u"Second document", path=u"/u", content=u"The second document even better!")
+        writer.commit()
+
+    def search(self, word):
+        ix = self.ix
+        with ix.searcher() as searcher:
+            query = QueryParser("content", ix.schema).parse(word)
+            results = searcher.search(query)
+
+        return results
+
+engine = SearchEngine()
+
+def main(args: list) -> None:
+    # print(f'{args}')
+
+    # engine.index()
+    result = engine.search("first")
+
+    inspect(result)
+
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
+
