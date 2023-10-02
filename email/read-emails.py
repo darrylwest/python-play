224fb7c64bfab802cb80b8b90b49f49dfb117fe8
darryl west <darryl.west@raincitysoftware.com>
2023-10-02 10:23:20 -0700

    added verbose and all flags to cli

--- email/read-emails.py
+++ email/read-emails.py
@@ -2,29 +2,45 @@
 # dpw@plaza.localdomain
 # 2023-10-01 18:41:17
 
-import sys
-import os
-from rich import print, inspect
-import imaplib
 import email
-from email.header import decode_header
+import imaplib
+import os
+import sys
 import tomllib
-from pathlib import Path
 from dataclasses import dataclass
+from datetime import datetime
+from email.header import decode_header
+from pathlib import Path
+
+from rich import inspect, print
+
 
 @dataclass
 class Config:
     host: str
     user: str
     pw: str
+    folder: str
+    search: str
 
     @classmethod
     def from_dict(cls, cfg: dict):
         return cls(
-            host = cfg.get('EMAIL_HOST'),
-            user = cfg.get('EMAIL_USER'),
-            pw = cfg.get('EMAIL_PW'),
+            host=cfg.get("EMAIL_HOST"),
+            user=cfg.get("EMAIL_USER"),
+            pw=cfg.get("EMAIL_PW"),
+            folder=cfg.get("FOLDER", "INBOX"),
+            search=cfg.get("SEARCH", "UNSEEN"),
         )
+
+
+@dataclass
+class EmailResponse:
+    ts: datetime
+    send_by: str
+    sent_at: str
+    subject: str
+    body: str
 
 
 def read_config(filename: str):
@@ -34,52 +50,61 @@ def read_config(filename: str):
     return data
 
 
-def read_all(ctx: Config):
+def read_all(ctx: Config) -> list[EmailResponse]:
     mb = imaplib.IMAP4_SSL(ctx.host)
     mb.login(ctx.user, ctx.pw)
     # typ, data = mb.search(None, 'UNSEEN')
-    mb.select('inbox')
-    typ, data = mb.search(None, 'ALL')
+    mb.select(ctx.folder)
+    typ, data = mb.search(None, ctx.search)
 
-    # typ, data = mb.search(None, 'UNSEEN')
-    # print(f'Type: {typ}')
+    emails = []
 
     for num in data[0].split():
-        typ, data = mb.fetch(num, '(RFC822)')
+        typ, data = mb.fetch(num, "(RFC822)")
         for resp_part in data:
             if isinstance(resp_part, tuple):
                 msg = email.message_from_bytes(resp_part[1])
-                mfrom = msg['from']
-                subject = msg['subject']
-                dt = msg['date']
+                print(msg)
+
+                # resp = EmailResponse()
+                mfrom = msg["from"]
+                subject = msg["subject"]
+                dt = msg["date"]
 
                 # TODO(dpw): create a rich table...
-                print(f'From: {mfrom}')
-                print(f'Sent: {dt}')
-                print(f'Subj: [green3]{subject}')
+                print(f"From: {mfrom}")
+                print(f"Sent: {dt}")
+                print(f"Subj: [green3]{subject}")
 
                 for part in msg.walk():
-                    if part.get_content_type() == 'text/plain':
-                        body = part.as_string().split('\n')
-                        text = ''.join([f'{tx}\n' for tx in body[1:] if tx != ''])
-                        print(f'Body: [green3]{text}')
-
+                    if part.get_content_type() == "text/plain":
+                        body = part.as_string().split("\n")
+                        text = "".join([f"{tx}\n" for tx in body[1:] if tx != ""])
+                        print(f"Body: [green3]{text}")
 
     mb.close()
     mb.logout()
 
+    return emails
+
+
 def main(args: list) -> None:
     # print(f'{args}')
     cfg = read_config("email/config.toml")
-    dpw500 = Config.from_dict(cfg.get('dpw500'))
-    dpw = Config.from_dict(cfg.get('dpw'))
+    dpw500 = Config.from_dict(cfg.get("dpw500"))
+    dpw = Config.from_dict(cfg.get("dpw"))
 
-    print(dpw500)
-    print(dpw)
+    if "--all" in args:
+        dpw500.search = "ALL"
+        dpw.search = "ALL"
+
+    if "--verbose" in args:
+        print(dpw500)
+        print(dpw)
 
     read_all(dpw500)
     read_all(dpw)
 
-if __name__ == '__main__':
-    main(sys.argv[1:])
 
+if __name__ == "__main__":
+    main(sys.argv[1:])
--- utils/send-smtp.py
+++ utils/send-smtp.py
@@ -8,18 +8,20 @@ import smtplib
 from email.message import EmailMessage
 from datetime import datetime
 
+
 def compose():
     msg = EmailMessage()
-    msg.set_content(f'this is my message created at {datetime.now()}.')
-    msg['Subject'] = 'A simple message...'
-    msg['From'] = 'dpw@raincitysoftware.com'
-    msg['To'] = 'dpw500@raincitysoftware.com'
+    msg.set_content(f"this is my message created at {datetime.now()}.")
+    msg["Subject"] = "A simple message..."
+    msg["From"] = "dpw@raincitysoftware.com"
+    msg["To"] = "dpw500@raincitysoftware.com"
 
-    s = smtplib.SMTP('smtp.dreamhost.com')
+    s = smtplib.SMTP("smtp.dreamhost.com")
+
 
 def main(args: list) -> None:
-    print(f'{args}')
+    print(f"{args}")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main(sys.argv[1:])
-
