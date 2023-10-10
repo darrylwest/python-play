b0243f38499cbdccfd30f002a1ad7d58aa2a8ce1
darryl west <darryl.west@raincitysoftware.com>
2023-10-10 11:06:57 -0700

    refactored to enable a group of tasks to be run in parallel

--- concurrency/async_random.py
+++ concurrency/async_random.py
@@ -4,19 +4,30 @@
 
 import asyncio
 import random
+from time import time
+from rich import inspect
 
 
 async def show(number: int):
-    delay = random.random()
+    delay = random.random() + 2.0
     await asyncio.sleep(delay)
 
     print(f"{number=}, {delay=}", flush=True)
+    return delay
 
 
 async def loop():
-    for n in range(10):
-        await show(n)
+    count = 20
+    print(f'\nThis script runs {count} jobs in parallel then awaits for all of them to be completed...\n', flush=True)
 
+    t0 = time()
+    jobs = [show(n) for n in range(10)]
+    res = await asyncio.gather(*jobs)
+    t1 = time()
+
+    # inspect(res)
+    print(f'elapsed: {t1-t0}')
+    print(f'sum....: {sum(res)}')
 
 if __name__ == "__main__":
     asyncio.run(loop())
