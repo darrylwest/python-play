87f83a8dcd8d790c7dd25294ec9b69461f6a942f
darryl west <darryl.west@raincitysoftware.com>
2023-10-18 12:23:10 -0700

    cython

---
+++ utils/timeit-tests.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# dpw@plaza.localdomain
+# 2023-10-18 17:52:46
+
+# ref: https://docs.python.org/3/library/timeit.html
+
+import sys
+from rich import print
+from timeit import timeit
+
+def main(args: list) -> None:
+    # print(f'{args}')
+
+    t1 = timeit('"-".join(str(n) for n in range(100))', number=10000)
+    print(f'loop  :{t1}')
+
+    t2 = timeit('"-".join([str(n) for n in range(100)])', number=10000)
+    print(f'comp():{t2}')
+
+    t3 = timeit('"-".join(map(str, range(100)))', number=10000)
+    print(f'map() :{t3}')
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
+
