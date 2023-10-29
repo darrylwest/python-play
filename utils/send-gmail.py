8756edb468d7285247ae6d4e502f4c0a32bc06f6
darryl west <darryl.west@raincitysoftware.com>
2023-10-29 12:13:36 -0700

    gmail creds and send

--- utils/send-gmail.py
+++ utils/send-gmail.py
@@ -2,23 +2,37 @@
 # dpw@plaza.localdomain
 # 2023-07-26 15:13:54
 
-from gmail.gmail import gmail
+import base64
+import os
+
+from gmail import GMail, Message
 
 # need to create a google app-passkey to use this
 
 
+def read_creds():
+    b64 = os.getenv("EMAIL_CREDS")
+    creds = base64.decodebytes(b64.encode())
+
+    id, pw = creds.split(b":")
+
+    return (id.decode(), pw.decode())
+
+
 def send_gmail(subject, body, to):
-    email = gmail()
-    # email_id =
-    # email_pw =
-    email.login(email_id, email_pw)
-    email.receiver_mail(to)
-    email.send_mail(subject, body)
+    id, pw = read_creds()
+
+    gmail = GMail(id, pw)
+    msg = Message(subject, to=to, text=body)
+    gmail.send(msg)
+
     print(f"message {subject} sent to: {to}")
 
 
 if __name__ == "__main__":
     # read the id and pw from local store...
-    print("not ready yet...")
+    # print("not ready yet...")
 
-    # send_gmail("my test message", "this is test message # 1...", "darryl.west@raincitysoftware.com")
+    send_gmail(
+        "my test message", "this is test message # 2...", "dpw500@raincitysoftware.com"
+    )
